31a39d69eb56b618a1f5fbef62ece8f04cbaf1cf
jordanfaroz <128454123+jordanfaroz@users.noreply.github.com>
2023-07-11 18:32:38 +0530

    Add files via upload

---
+++ lambda/lambda1.py
@@ -0,0 +1,3 @@
+ans= lambda a: "positive" if a>0 else "negative"
+no = int(input("enter no: "))
+print(ans(no))
---
+++ lambda/lambda2.py
@@ -0,0 +1,5 @@
+ans= lambda a,b: a*b
+no = int(input("enter first no "))
+no1 = int(input("enter second no "))
+
+print(ans(no,no1))
